9266c142a202083d82e4c0bbc81c9433764ea189
raymonreddd <raymonreddington40@gmail.com>
2024-08-26 19:50:15 +0530

    feat: encoding the text to file using bytes and gob package
    Fix: exported fields of huffmanNode struct to be used by packages, and casing

--- chal3/main.go
+++ chal3/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"container/heap"
+	"encoding/gob"
 	"fmt"
+	"log"
 	"os"
-	"sort"
 )
 
 /*
@@ -17,8 +19,8 @@ COmperession tool
 
 // Huffman Node
 type HuffmanNode struct {
-	char      rune
-	frequency int
+	Char      rune
+	Frequency int
 	left      *HuffmanNode
 	right     *HuffmanNode
 }
@@ -31,7 +33,7 @@ func (pq PriorityQueue) Len() int { return len(pq) }
 // since we want the order to be in ascending.
 // Pop first 2 join them, and continue
 func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i].frequency < pq[j].frequency
+	return pq[i].Frequency < pq[j].Frequency
 }
 
 func (pq PriorityQueue) Swap(i, j int) {
@@ -54,6 +56,7 @@ func (pq *PriorityQueue) Pop() interface{} {
 
 func main() {
 	file := readFile("test.txt")
+	defer file.Close()
 
 	// Step 1 - frequency map for each rune or char
 	freq_map, err := genFrequencyMap(file)
@@ -64,14 +67,40 @@ func main() {
 	printTree(tree, 0)
 
 	// Step 3 -  Prefix code table for each rune the respective prefixed binary code
-	code_table := make(map[rune]string)
-	generateCodes(tree, "", code_table)
+	prefix_code_table := make(map[rune]string)
+	generatePrefixTableMap(tree, "", prefix_code_table)
 
 	fmt.Println("Generated Huffman Codes:")
-	for char, code := range code_table {
+	for char, code := range prefix_code_table {
 		fmt.Printf("%q: %s\n", char, code)
 	}
 
+	// Step 4a - get all text from file
+	text := getTextFromFile(file)
+
+	// step 4b - main enocde
+	encoded_text := encodeText(text, prefix_code_table)
+
+	if err := writeEncodedFile("encoded.huff", tree, encoded_text); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func getTextFromFile(file *os.File) string {
+
+	var text string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		text += line
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file in getTextFromFile:", err)
+	}
+
+	return text
 }
 
 // using dfs to print the tree
@@ -81,7 +110,7 @@ func printTree(node *HuffmanNode, depth int) {
 	}
 
 	if node.left == nil && node.right == nil {
-		fmt.Printf("Node character: %c at depth: %d with frequency: %d \n", node.char, depth, node.frequency)
+		fmt.Printf("Node character: %c at depth: %d with frequency: %d \n", node.Char, depth, node.Frequency)
 		return
 	}
 
@@ -104,7 +133,6 @@ func readFile(file_name string) *os.File {
 
 func genFrequencyMap(file *os.File) (map[rune]int, error) {
 	reader := bufio.NewReader(file)
-	defer file.Close()
 
 	freq_map := make(map[rune]int)
 
@@ -120,19 +148,10 @@ func genFrequencyMap(file *os.File) (map[rune]int, error) {
 		freq_map[char]++
 	}
 
-	keys := make([]rune, 0, len(freq_map))
-
 	// ch is character and frq is frequency
 	for ch, frq := range freq_map {
 		fmt.Printf("Character: %c %d \n", ch, frq)
-		keys = append(keys, ch)
 	}
-
-	sort.SliceStable(keys, func(i, j int) bool {
-		return freq_map[keys[i]] < freq_map[keys[j]]
-	})
-
-	fmt.Println(keys)
 	return freq_map, nil
 }
 
@@ -145,7 +164,7 @@ func buildHuffmanTree(frequencies map[rune]int) *HuffmanNode {
 	heap.Init(&pq)
 
 	for char, freq := range frequencies {
-		heap.Push(&pq, &HuffmanNode{char: char, frequency: freq})
+		heap.Push(&pq, &HuffmanNode{Char: char, Frequency: freq})
 	}
 
 	// As long as there are 2 nodes left in heap, keep doing below
@@ -157,7 +176,7 @@ func buildHuffmanTree(frequencies map[rune]int) *HuffmanNode {
 
 		// merge them and them again to the heap, where it will reorganized in it
 		merged := &HuffmanNode{
-			frequency: left.frequency + right.frequency,
+			Frequency: left.Frequency + right.Frequency,
 			left:      left,
 			right:     right,
 		}
@@ -173,17 +192,54 @@ func buildHuffmanTree(frequencies map[rune]int) *HuffmanNode {
 /*
 Function to genretate prefix table, where rune - binary output of that rune (or character, pick your vice)
 */
-func generateCodes(node *HuffmanNode, prefix string, code_table map[rune]string) {
+func generatePrefixTableMap(node *HuffmanNode, prefix string, code_table map[rune]string) {
 	if node == nil {
 		return
 	}
 
 	// If its leaf node, then its a rune, assign the rune to the prefix in code_table
 	if node.left == nil && node.right == nil {
-		code_table[node.char] = prefix
+		code_table[node.Char] = prefix
 	}
 
 	// left edge will keep adding 0 and vice versa
-	generateCodes(node.left, prefix+"0", code_table)
-	generateCodes(node.right, prefix+"1", code_table)
+	generatePrefixTableMap(node.left, prefix+"0", code_table)
+	generatePrefixTableMap(node.right, prefix+"1", code_table)
+}
+
+func encodeText(text string, prefix_code_table map[rune]string) string {
+	var encoded_text string
+
+	//extract chars from string and use the map to get the ecoded bit value
+	for _, char := range text {
+		encoded_text += prefix_code_table[char]
+	}
+	return encoded_text
+}
+
+func writeEncodedFile(filename string, tree *HuffmanNode, encoded_text string) error {
+	var buf bytes.Buffer
+	encoder := gob.NewEncoder(&buf)
+	if err := encoder.Encode(tree); err != nil {
+		return err
+	}
+
+	encoded_data := buf.Bytes()
+	encoded_data = append(encoded_data, []byte(encoded_text)...)
+
+	// Open the file for writing
+	/*
+		file is opened with the os.O_WRONLY (write-only) flag,
+		the os.O_CREATE flag (which creates the file if it doesn’t exist),
+		and the os.O_TRUNC flag (which truncates the file if it already exists).
+
+		0644 is permissions -  (readable and writable by the owner, readable by others).
+	*/
+	file, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	check(err)
+	defer file.Close()
+
+	// Write the encoded data to the file
+	_, err = file.Write(encoded_data)
+	return err
 }
