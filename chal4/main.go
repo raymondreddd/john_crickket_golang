58b3a540ce0b2983f5a374ab63d183a47238c502
raymonreddd <raymonreddington40@gmail.com>
2024-08-27 23:11:04 +0530

    feat: basic input reading and separating using pipe op, and validation

--- chal4/main.go
+++ chal4/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
 	"fmt"
 	"os"
@@ -9,7 +10,61 @@ import (
 
 func main() {
 
-	filename := "sample.tsv"
+	// Taking user input
+	input_reader := bufio.NewReader(os.Stdin)
+
+	input, err := input_reader.ReadString('\n')
+	check(err)
+
+	// split into multiple user commands by pipe operator
+	commands := strings.Split(input, "|")
+
+	if len(commands) == 0 {
+		return
+	}
+
+	for index, command := range commands {
+		snippets := strings.Split(command, " ")
+
+		if index == 0 {
+			// check if 1st word is 'cut'
+			if snippets[0] != "cut" {
+				fmt.Print("Unknown command, use cut")
+				os.Exit(1)
+			}
+
+			// check for -f
+			if len(snippets) < 3 || !strings.HasPrefix(snippets[1], "-f") {
+				fmt.Print("Unknown command, use cut -f1 sample.tsv")
+				os.Exit(1)
+			}
+
+			field := snippets[1][2:]
+			fmt.Print("Field number:", field)
+
+		}
+
+	}
+
+}
+
+func readFile(filename string) *os.File {
+	file, err := os.Open(filename)
+	check(err)
+
+	return file
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Print("error:", err)
+		os.Exit(1)
+	}
+}
+
+func extractFields(filename string, field int) []string {
+	var res []string
+
 	// Read csv file
 	file := readFile(filename)
 	defer file.Close()
@@ -29,18 +84,5 @@ func main() {
 		fmt.Println(record)
 	}
 
-}
-
-func readFile(filename string) *os.File {
-	file, err := os.Open(filename)
-	check(err)
-
-	return file
-}
-
-func check(err error) {
-	if err != nil {
-		fmt.Print("error:", err)
-		os.Exit(1)
-	}
+	return res
 }
