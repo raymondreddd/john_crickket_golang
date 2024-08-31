3c39bffdea7bde6694884c8d6c856d32e0795d87
raymonreddd <raymonreddington40@gmail.com>
2024-08-31 22:19:24 +0530

    feat: use mux for server and address flags

--- chal11/main.go
+++ chal11/main.go
@@ -1,25 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Construct the response
 	response := fmt.Sprintf("Requested path: %s", r.URL.Path)
 
-	// Send the response
 	fmt.Fprintln(w, response)
 }
 
 func main() {
-	// Register the handler function for all paths
-	http.HandleFunc("/", handler)
 
-	// Start the server on port 80
-	fmt.Println("Server is listening on 127.0.0.1:80")
-	err := http.ListenAndServe("127.0.0.1:80", nil)
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+
+	log.Printf("Server is listening on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
