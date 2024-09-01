31609c8c3b7b5582be2af9e2d25c9c263632fa1e
raymonreddd <raymonreddington40@gmail.com>
2024-09-01 21:11:12 +0530

    feat: serve to use custom server with config and custom mux

--- chal11/cmd/web/main.go
+++ chal11/cmd/web/main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
-
-	addr := flag.String("addr", ":4000", "HTTP network address")
-	flag.Parse()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
 	log.Printf("Server is listening on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+
+	// Use ListenAndServe with a custom server to set timeouts
+	server := &http.Server{
+		Addr:         *addr,
+		Handler:      mux,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  30 * time.Second,
+	}
+
+	err := server.ListenAndServe()
 	if err != nil {
-		fmt.Println("Error starting server:", err)
+		log.Fatalf("Error starting server: %v", err)
 	}
 }
